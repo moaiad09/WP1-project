f4ebc6a372fa8b0a075dc0dac70eef5f300c7d82
BatoulAlAni <anbatoul@gmail.com>
2025-05-18 17:17:56 +0300

    fun page without loading quote file

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  // slideToggle
+  $('#toggleBox').click(function () {
+    $('#funBox').slideToggle('fast', function () {
+      const isVisible = $('#funBox').is(':visible');
+      $('#toggleBox').text(isVisible ? 'Hide Box' : 'Show Box');
+    });
+  });
+
+  // fadeIn / fadeOut
+  $('#toggleImage').click(function () {
+    $('#funImage').fadeToggle();
+  });
+
+  // AJAX .load()
+  $('#loadQuote').click(function () {
+    $('#jokeText').load('quote.txt');
+  });
+});
